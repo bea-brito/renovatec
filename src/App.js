5de211e25f0f59dfe114818eb3e8ab511af8085b
Gabriel Feitosa <gabrielfcb13@gmail.com>
2024-04-03 11:44:36 -0300

    Criado pagina de menu para testes de autentificacao

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./componentes/loginPage/login";
 import Senha from "./componentes/redefinirSenha/senha";
 import Cadastro from "./componentes/cadastrar/cadastro";
+import Menu from "./componentes/menu/menu";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/senha" element={<Senha />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/menu" element={<Menu />} />
           <Route path="/" element={<Login />} />
         </Routes>
       </div>
---
+++ src/componentes/menu/menu.js
@@ -0,0 +1,28 @@
+import Botao from "../botao/botao";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <div className="w-full h-screen flex items-start">
+      <div className="w-full flex flex-col p-20">
+        <h3 className="text-2xl font-semibold mb-4 text-yellow-500">
+          Seja Bem Vindo!
+        </h3>
+
+        <div className="w-full flex items-center justify-center">
+          <Link
+            to="/"
+            className="text-sm cursor-pointer whitespace-nowrap font-medium underline underline-offset-2 hover:text-yellow-500"
+          >
+            <Botao className="w-full mt-2 bg-black text-white py-2 px-4 rounded hover:bg-yellow-500">
+              {" "}
+              Voltar{" "}
+            </Botao>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
