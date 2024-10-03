69879bb1de0cfca1885eaad33c594a2e6858af84
Gabriel Feitosa <gabrielfcb13@gmail.com>
2024-10-03 15:22:07 -0300

    Corrigido erro na importação do arquivo env.js

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,8 +1,11 @@
+const {
+  REACT_APP_SUPABASE_URL,
+  REACT_APP_SUPABASE_ANON_KEY,
+} = require("./env");
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://alqpreifzqbsjktldcqd.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFscXByZWlmenFic2prdGxkY3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExMzcwMDgsImV4cCI6MjAyNjcxMzAwOH0.HAE9HVZkGSc0gYqn-OqnAlR9PmxsyUnfAhO3lr8_nTI";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  REACT_APP_SUPABASE_URL,
+  REACT_APP_SUPABASE_ANON_KEY
+);
 module.exports = supabase;
